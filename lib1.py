1474ab9d10e900af20d6f7603c6986ab8ffb13df
JesusEmilioCastroCorona <24040005@utculiacan.edu.mx>
2025-08-22 11:53:20 -0700

    Update lib1.py

---
+++ lib1.py
@@ -0,0 +1,16 @@
+# libreria1.py
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División entre cero"
