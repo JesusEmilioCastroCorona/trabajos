f8146ff0e3de53737d06e9f38202efc63a7d2012
JesusEmilioCastroCorona <24040005@utculiacan.edu.mx>
2025-08-22 11:52:56 -0700

    Update lib2.py

---
+++ lib2.py
@@ -0,0 +1,16 @@
+# libreria2.py
+
+def banner(texto):
+    linea = "*" * (len(texto) + 4)
+    return f"{linea}\n* {texto} *\n{linea}"
+
+def corazon():
+    return """
+     **   **
+    **** ****
+    *********
+     *******
+      *****
+       ***
+        *
+    """
